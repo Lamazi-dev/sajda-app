dba701fb73b31b3c20534fc3c6d4d69211e2a07c
lamazi-dev <lamazicarry@gmail.com>
2026-03-23 22:52:13 +0300

    Add initial Today screen

--- android/sajda-app/app/src/main/java/com/sajda/app/MainActivity.kt
+++ android/sajda-app/app/src/main/java/com/sajda/app/MainActivity.kt
@@ -3,45 +3,16 @@ package com.sajda.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.sajda.app.feature.today.TodayScreen
 import com.sajda.app.ui.theme.SajdaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             SajdaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                TodayScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SajdaTheme {
-        Greeting("Android")
     }
 }
---
+++ android/sajda-app/app/src/main/java/com/sajda/app/feature/today/TodayScreen.kt
@@ -0,0 +1,21 @@
+package com.sajda.app.feature.today
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun TodayScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Sajda Today Screen")
+        Text(text = "Prayer Times & Qibla")
+    }
+}
